c332a7edf589a05d93b63010fd51ccd137e65c62
Tien Le <levantecr7@gmail.com>
2023-09-13 17:30:01 +0700

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    fist_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return(f"{self.fist_name} {self.last_name}")
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     #check if user is logged in
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,7 +20,7 @@ def home(request):
             messages.error(request, 'Error logging in - please try again')  # Changed to error
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 # Create your views here.
 
@@ -30,4 +32,61 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.error(request, 'You must be logged in to view that')
+        return redirect('home')
+    
+def delete_record(request, pk):
+     if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        customer_record.delete()
+        messages.success(request, 'Record has been deleted')
+        return redirect('home')
+     
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record=form.save()
+                messages.success(request, 'Record has been added')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, 'You must be logged in to view that')
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=customer_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record has been updated')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.error(request, 'You must be logged in to view that')
+        return redirect('home')
